0107b98ca1b4a654a434a0de1c95c5969acf46ab
Adrian Potter <Adrian.Potter@tessella.com>
2018-05-03 16:21:00 +0100

    Have a look at what's being built

--- docs/make_doc.sh
+++ docs/make_doc.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -o errexit
 env PYTHONPATH=.. make html
+ls -R _build/html
 rm -fr /isis/www/doxygen/shared_instrument_scripts/sphinx
 cp -r _build/html /isis/www/doxygen/shared_instrument_scripts/sphinx
