222065959bede45855a275f5abecf35a78c7ad4d
Adrian Potter <Adrian.Potter@tessella.com>
2018-05-03 16:05:54 +0100

    Make missing dirs if necessary

--- docs/make_doc.sh
+++ docs/make_doc.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -o errexit
 env PYTHONPATH=.. make html
+if [ ! -d /isis/www/doxygen/shared_scripting/ ]; then
+  mkdir -p /isis/www/doxygen/shared_scripting/
+fi
 rm -fr /isis/www/doxygen/shared_scripting/sphinx
 cp -r _build/html /isis/www/doxygen/shared_scripting/sphinx
