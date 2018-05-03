5e4eee6ab63e09069ea7d88069eb603038161440
Adrian Potter <Adrian.Potter@tessella.com>
2018-05-03 16:28:23 +0100

    Add test script to check documentation build

---
+++ general/test.py
@@ -0,0 +1,16 @@
+def test(arg1, arg2):
+	"""
+	Test method to check if the shared instrument scripting documentation
+	build is working.
+
+    Args:
+        arg1: The first argument
+		arg2: The second argument
+		
+	Returns:
+		None
+
+    Examples:
+        >>> test("Cheese", "Biscuits")
+	"""
+	return None
