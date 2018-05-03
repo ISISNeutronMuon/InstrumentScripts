80c36749a5034a351887e870e573ccb5675e9df8
Adrian Potter <Adrian.Potter@tessella.com>
2018-05-03 16:19:14 +0100

    Script doesn't have permission to create this dir

--- docs/make_doc.sh
+++ docs/make_doc.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -o errexit
 env PYTHONPATH=.. make html
-if [ ! -d /isis/www/doxygen/shared_instrument_scripts/ ]; then
-  mkdir -p /isis/www/doxygen/shared_instrument_scripts/
-fi
 rm -fr /isis/www/doxygen/shared_instrument_scripts/sphinx
 cp -r _build/html /isis/www/doxygen/shared_instrument_scripts/sphinx
