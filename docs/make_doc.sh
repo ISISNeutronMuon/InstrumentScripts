0055e1442b24936e23de8e5485cfc66f159dc97f
Adrian Potter <Adrian.Potter@tessella.com>
2018-05-04 11:49:12 +0100

    Let's see what we're making in docs...

--- docs/make_doc.sh
+++ docs/make_doc.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -o errexit
 sphinx-autogen -o source/ shared_instrument_scripts.rst
+ls -R .
 env PYTHONPATH=.. make html
 ls -R _build/html
 rm -fr /isis/www/doxygen/shared_instrument_scripts/sphinx
