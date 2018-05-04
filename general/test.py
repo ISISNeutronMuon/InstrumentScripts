c4f5920eba615b7fd49317a65099cdad53bf78d2
Adrian Potter <Adrian.Potter@tessella.com>
2018-05-04 14:31:42 +0100

    Resurrect test script again

---
+++ general/test.py
@@ -0,0 +1,16 @@
+print "Haha! Sneaky test execution code ran"
+
+def test(arg1, arg2):
+	"""
+	Test method to check if the shared instrument scripting documentation
+	build is working.
+    Args:
+        arg1 (string, optional): The first argument
+        arg2 (string, optional): The second argument
+		
+	Returns:
+		None
+    Examples:
+        >>> test("Cheese", "Biscuits")
+	"""
+	return None
