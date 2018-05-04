e99e0bef78dc97874dc7730feef2cee3e95604c1
Adrian Potter <Adrian.Potter@tessella.com>
2018-05-04 14:17:06 +0100

    Readd test file to check sphinx mechanism

---
+++ general/test.py
@@ -0,0 +1,20 @@
+print "Haha! Sneaky test execution code ran"
+ARG1 = "Dynamic arg 1"
+ARG2 = "Dynamic arg 2"
+
+def test(arg1, arg2):
+	"""
+	Test method to check if the shared instrument scripting documentation
+	build is working.
+
+    Args:
+        arg1 (string, optional): {}
+        arg2 (string, optional): {}
+		
+	Returns:
+		None
+
+    Examples:
+        >>> test("Cheese", "Biscuits")
+	""".format(ARG1, ARG2)
+	return None
