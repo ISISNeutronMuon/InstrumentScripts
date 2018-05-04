0cbdc947403abd5e9afaed0331ad3a01ffd8c311
Adrian Potter <Adrian.Potter@tessella.com>
2018-05-04 14:34:38 +0100

    Restore general from raw

--- general/test.py
+++ general/test.py
@@ -1,16 +1,18 @@
 print "Haha! Sneaky test execution code ran"
 
 def test(arg1, arg2):
-    """
-    Test method to check if the shared instrument scripting documentation
-    build is working.
+	"""
+	Test method to check if the shared instrument scripting documentation
+	build is working.
+
     Args:
         arg1 (string, optional): The first argument
         arg2 (string, optional): The second argument
-        
-    Returns:
-        None
+		
+	Returns:
+		None
+
     Examples:
         >>> test("Cheese", "Biscuits")
-    """
-    return None
+	"""
+	return None
