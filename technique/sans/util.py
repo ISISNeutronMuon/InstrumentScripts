18a3332c5c4b8567dd79321497b51175ae7fc709
Adam Washington <adam.washington@stfc.ac.uk>
2018-10-29 10:23:55 +0000

    Add debugging statement for sans scripting problem

--- technique/sans/util.py
+++ technique/sans/util.py
@@ -45,6 +45,7 @@ def dae_setter(suffix, measurement_type):
             """Memoize the dae mode"""
             request = inner.__name__[10:]
             if request == self._dae_mode:  # pylint: disable=protected-access
+                debug("instrument was already set for {}".format(request)
                 return
             inner(self, *args, **kwargs)
             info("Setup {} for {}".format(type(self).__name__,
