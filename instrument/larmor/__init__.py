f3ce9265da7916c242ed0569e1b59d5fc2e5adb7
Adam Washington <adam.washington@stfc.ac.uk>
2018-10-29 10:16:52 +0000

    create generic larmor import module

---
+++ instrument/larmor/__init__.py
@@ -0,0 +1,7 @@
+
+from instrument.larmor.sans import *
+from instrument.larmor.scans import scan, ascan, rscan, dscan, pol_measure, \
+    fast_pol_measure, detector_trans
+from general.scans.detector import specific_spectra
+from general.scans.fit import *
+from general.scans.motion import populate
