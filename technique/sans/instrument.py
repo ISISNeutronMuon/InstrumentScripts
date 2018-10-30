fce7fcfc0e495d1328fb2b57c47858c778fa70e7
Adam Washington <adam.washington@stfc.ac.uk>
2018-10-30 13:41:54 +0000

    Let the sans instruments share sample changer positions

--- technique/sans/instrument.py
+++ technique/sans/instrument.py
@@ -8,7 +8,7 @@ any generic scripts.
 
 """
 
-from abc import ABCMeta, abstractmethod, abstractproperty
+from abc import ABCMeta, abstractmethod
 from logging import info, warning
 from six import add_metaclass
 from .genie import gen
@@ -102,11 +102,18 @@ class ScanningInstrument(object):
             gen.change_tcb(**tcb)
         gen.change_finish()
 
-    @abstractproperty
-    def _poslist(self):  # pragma: no cover
-        """The list of named positions that the instrument can run through in
-        the sample changer"""
-        return []
+    _poslist = ['AB', 'BB', 'CB', 'DB', 'EB', 'FB', 'GB', 'HB', 'IB', 'JB',
+                'KB', 'LB', 'MB', 'NB', 'OB', 'PB', 'QB', 'RB', 'SB', 'TB',
+                'AT', 'BT', 'CT', 'DT', 'ET', 'FT', 'GT', 'HT', 'IT', 'JT',
+                'KT', 'LT', 'MT', 'NT', 'OT', 'PT', 'QT', 'RT', 'ST', 'TT',
+                '1CB', '2CB', '3CB', '4CB', '5CB', '6CB', '7CB',
+                '8CB', '9CB', '10CB', '11CB', '12CB', '13CB', '14CB',
+                '1CT', '2CT', '3CT', '4CT', '5CT', '6CT', '7CT',
+                '8CT', '9CT', '10CT', '11CT', '12CT', '13CT', '14CT',
+                '1WB', '2WB', '3WB', '4WB', '5WB', '6WB', '7WB',
+                '8WB', '9WB', '10WB', '11WB', '12WB', '13WB', '14WB',
+                '1WT', '2WT', '3WT', '4WT', '5WT', '6WT', '7WT',
+                '8WT', '9WT', '10WT', '11WT', '12WT', '13WT', '14WT']
 
     @staticmethod
     def _needs_setup():
