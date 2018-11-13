c0d5848705b826858a5ed16af43ec71b173d3f8e
Adam Washington <rprospero@gmail.com>
2018-11-13 16:51:04 +0000

    Replay scan (#10)
    This pr adds the ability to replay scans that have already been performed. The latest_scan function pulls the most recent scan while the ReplayScan class allows for the creation of arbitrary pseudoscans.
    
    Everything has been tested and linted

--- test.sh
+++ test.sh
@@ -4,7 +4,6 @@ set -e
 
 flake8 --exclude technique/sans/test general instrument technique
 pylint --ignore test general instrument technique
-# # coverage run --source scans -m doctest doc/source/tutorial.rst doc/source/instrument.rst
 coverage run --source=general,instrument,technique -m unittest discover
 coverage html
 cd doc
