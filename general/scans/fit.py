7331a5ab261cdaf6017083a43683eac8d95de33a
Adam Washington <adam.washington@stfc.ac.uk>
2019-05-13 12:48:25 +0100

    Fix centre of mass fit

--- general/scans/fit.py
+++ general/scans/fit.py
@@ -516,7 +516,7 @@ class CentreOfMassFit(Fit):
         warnings.simplefilter("ignore", RuntimeWarning)
 
     def fit(self, x, y, err):
-        if not x or not y:
+        if not (x and y.size and err.size):
             return [np.nan]
 
         raw_data = np.array([
