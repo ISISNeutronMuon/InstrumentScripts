2409795df5ed5b0f1d98707f4e7e7d179863804b
Adam Washington <adam.washington@stfc.ac.uk>
2019-10-10 14:50:09 +0100

    Add import and call graphs to the documentation

---
+++ doc/source/graphs.rst
@@ -0,0 +1,18 @@
+Module Import Graph
+===================
+
+  .. images:: call.dot.pdf
+     :alt: The dependency graph for module imports
+
+Scans Call Graph
+================
+
+  .. image:: general_instrument.dot.pdf
+     :alt: The paths of calls in the Scans library
+
+
+Sans Call Graph
+================
+
+  .. image:: technique_instrument.dot.pdf
+     :alt: The paths of calls in the Sans library
--- test.sh
+++ test.sh
@@ -6,6 +6,13 @@ flake8 --exclude technique/sans/test general instrument technique
 pylint --ignore test general instrument technique
 coverage run --source=general,instrument,technique -m unittest discover
 coverage html
+python doc/call.py > call.dot
+python doc/functions.py general instrument
+python doc/functions.py technique instrument
+dot -Tpdf -O *.dot
+mv general_instrument.dot.pdf doc/source/
+mv technique_instrument.dot.pdf doc/source/
+mv call.dot.pdf doc/source/
 cd doc
 make html
 make latexpdf
