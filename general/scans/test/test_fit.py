c28d99484171fcd3321c408a9d0868dd8d0ae610
Alistair McGann <alistair.mcgann@tessella.com>
2020-05-11 16:46:43 +0100

    Add tests

--- general/scans/test/test_fit.py
+++ general/scans/test/test_fit.py
@@ -1,8 +1,68 @@
 import unittest
-from mock import Mock
 import numpy as np
 
-from ..fit import PeakFit
+from ..fit import PeakFit, PolyFit, GaussianFit, Fit
+
+
+class MinimalFit(Fit):
+    """
+    A fitting class which is used to test the functionality of the Fit metaclass
+    """
+    def fit(self, x, y, err):
+        return None
+
+    def get_y(self, x, fit):
+        """
+        Evaluate the model described by fit at point x
+        """
+        return np.polyval(fit, x)
+
+    def readable(self, fit):
+        return None
+
+
+class ScansFitTest(unittest.TestCase):
+    """
+    Tests for the base Fit class
+    """
+    # Create a new class which inherits off Fit and call the method on the new class, with the required methods returning None
+    def setUp(self):
+        self.fitter = MinimalFit(1, "Minimal")
+
+    def test_GIVEN_fit_with_error_WHEN_quality_calculated_THEN_correct_fit_error_returned(self):
+        x = np.linspace(0.1, 20.0, 100)
+        # measured Y follows 2*x
+        real_y = np.polyval([2.0, 0.0], x)
+
+        # Make 'fit' 10% off
+        fit = np.poly1d([2.2, 0.0])
+
+        # Fit quality compares measurement errors to fit errors, so make 10% smaller than fit errors
+        errs = 0.01 * real_y
+
+        # fit quality is fit error squared
+        fit_error = np.sqrt(self.fitter.fit_quality(x, real_y, errs, fit))
+
+        self.assertAlmostEqual(fit_error, 10.0)
+
+
+class ScansPolyFitTest(unittest.TestCase):
+    """
+    Tests for the poly fit class
+    """
+    def setUp(self):
+        self.fitter = PolyFit(2)
+
+    def test_GIVEN_sample_polynomial_data_WHEN_fit_performed_THEN_correct_coefficients_returned(self):
+        test_polynomial = [-1.0, 0.1, 0.01]
+
+        test_x = np.linspace(-10, 10, 100)
+        test_y = np.polyval(test_polynomial, test_x)
+
+        fit = self.fitter.fit(test_x, test_y, 0.1*test_y)
+
+        for fit_coefficient, test_coefficient in zip(fit, test_polynomial):
+            self.assertAlmostEqual(fit_coefficient, test_coefficient)
 
 
 class ScansPeakFitTest(unittest.TestCase):
@@ -24,3 +84,29 @@ class ScansPeakFitTest(unittest.TestCase):
         test_y = test_function(test_x)
 
         self.assertAlmostEqual(self.fitter.fit(test_x, test_y, 10.0), test_peak_location, delta=1e-5)
+
+
+class GaussianFitTest(unittest.TestCase):
+    """
+    Tests for Gaussian fitting routines
+    """
+
+    def _gaussian(self, x, amplitude, centre, sigma, background):
+        """
+        Returns value of gaussian with supplied coefficients evaluated at x
+        """
+
+        return amplitude * np.exp(- np.power((x - background), 2)/np.power(2.0*sigma, 2)) + background
+
+    def setUp(self):
+        self.plotter = GaussianFit()
+
+    def test_GIVEN_sample_data_WHEN_guess_estimated_THEN_correct_estimate_returned(self):
+        test_sigma = 2.0
+        test_amplitude = 5.0
+        test_centre = 2.0
+        test_background = 10.0
+
+        test_x = np.linspace(0, 20, 100)
+
+        test_y = self._gaussian(test_x, test_amplitude, test_centre, test_sigma, test_background)
