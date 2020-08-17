2417eccc9c55a89e445f6d8aba9bb2613cbb025c
Thomas Lohnert <thomas.lohnert@stfc.ac.uk>
2020-08-17 15:15:34 +0100

    Update __init__.py

--- instrument/inter/__init__.py
+++ instrument/inter/__init__.py
@@ -1,4 +1,4 @@
-"""The base module for the POLREF beamline
+"""The base module for the INTER beamline
 
 Using a wildcard import will pull in most of the interesting
 commands for this instrument."""
