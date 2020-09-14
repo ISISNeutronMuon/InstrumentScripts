33a0bf1bc0164961e8035b2757e5925c0efe844f
Dominic Oram <dominic.oram@stfc.ac.uk>
2020-09-14 15:41:26 +0100

    Look at block value rather than alarm

--- general/utilities/restart_ioc_when_pv_in_alarm.py
+++ general/utilities/restart_ioc_when_pv_in_alarm.py
@@ -10,8 +10,8 @@ from server_common.helpers import register_ioc_start
 IOC_TO_RUN_IN = "BGRSCRPT_02"
 #BLOCK_TO_MONITOR = "SIMPLE_VAL"
 #IOC_TO_RESTART = ["SIMPLE"]
-BLOCK_TO_MONITOR = "ZFCNTRL_01:FIELD:MAGNITUDE"
-IOC_TO_RESTART = ["ZFCNTRL_01", "ZFMAGFLD_01"]
+BLOCK_TO_MONITOR = "field_ZF_status"
+IOC_TO_RESTART = ["ZFMAGFLD_01"]
 
 WAIT_BETWEEN_RESTARTS = 120
 WAIT_FOR_POLLING = 10
@@ -22,7 +22,7 @@ g.set_instrument(None)
 
 while True:
     block_details = g.cget(BLOCK_TO_MONITOR)
-    if not block_details["connected"] or block_details["alarm"] == "INVALID":
+    if block_details["value"] == "No new magnetometer data" or block_details["value"] == "Magnetometer data invalid":
         for ioc in IOC_TO_RESTART:
             print("Restarting {}".format(ioc))
             g.set_pv("CS:PS:{}:RESTART".format(ioc), 1, is_local=True)
