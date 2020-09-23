ca3d9c23bb432754333cfa304d0d46e15fe8671b
Jack Harper <jack.harper@stfc.ac.uk>
2020-09-23 12:13:48 +0100

    Making restart_ioc_when_pv_in_alarm() general function

--- general/utilities/restart_ioc_when_pv_in_alarm.py
+++ general/utilities/restart_ioc_when_pv_in_alarm.py
@@ -1,31 +1,31 @@
 import sys
 import os
-
 sys.path.insert(0, os.path.abspath(os.path.join(os.environ["KIT_ROOT"], "ISIS", "inst_servers", "master")))
-
 from genie_python import genie as g
 from time import sleep
 from server_common.helpers import register_ioc_start
+from typing import List
 
 IOC_TO_RUN_IN = "BGRSCRPT_02"
-BLOCK_TO_MONITOR = "field_ZF_status"
-IOC_TO_RESTART = ["ZFMAGFLD_01"]
 
-WAIT_BETWEEN_RESTARTS = 120
-WAIT_FOR_POLLING = 10
 
-register_ioc_start(IOC_TO_RUN_IN)
-
-g.set_instrument(None)
-
-while True:
-    block_details = g.cget(BLOCK_TO_MONITOR)
-    block_details_second_check = g.cget(BLOCK_TO_MONITOR)
-    # Check twice to make sure error is consistent
-    if all([pv_update["value"] in ["No new magnetometer data", "Magnetometer data invalid"]
-            for pv_update in [block_details, block_details_second_check]]):
-        for ioc in IOC_TO_RESTART:
-            print("Restarting {}".format(ioc))
-            g.set_pv("CS:PS:{}:RESTART".format(ioc), 1, is_local=True)
-        sleep(WAIT_BETWEEN_RESTARTS)
-    sleep(WAIT_FOR_POLLING)
+def restart_ioc_when_pv_in_alarm(block_to_monitor: str, iocs_to_restart: List[str], error_states: List[str],
+                                 wait_between_restarts: int = 120, wait_for_polling: int = 10):
+    """
+    Monitor and restart an IOC when a pv goes into a specified alarm state.
+    :param block_to_monitor: Block to monitor PV status.
+    :param iocs_to_restart: List of IOCs to restart if the block goes into an alarm.
+    :param error_states: List of error states that should cause the IOC to restart
+    :param wait_between_restarts: The time (seconds) to wait between restarts
+    :param wait_for_polling: The time (seconds) to wait between polling for the block's value
+    """
+    register_ioc_start(IOC_TO_RUN_IN)
+    g.set_instrument(None)
+    while True:
+        block_details = g.cget(block_to_monitor)
+        if block_details["value"] in error_states:
+            for ioc in iocs_to_restart:
+                print("Restarting {}".format(ioc))
+                g.set_pv("CS:PS:{}:RESTART".format(ioc), 1, is_local=True)
+            sleep(wait_between_restarts)
+        sleep(wait_for_polling)
