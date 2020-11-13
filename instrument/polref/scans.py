43be760662017eaa4a7c67cbe9f025908d2a3441
ThomasLohnert <thomas.lohnert@stfc.ac.uk>
2020-11-13 17:13:58 +0000

    Fix default detector & output dir

--- instrument/polref/scans.py
+++ instrument/polref/scans.py
@@ -29,13 +29,13 @@ class PolrefDefaultScan(Defaults):
     """
 
     # spectra definition based on gcl script.
-    _single_det_spectra = [create_spectra_definition(1, 100.0, 50000.0),  # Chopper pit
-                           create_spectra_definition(2, 100.0, 60000.0),  # Front of Blockhouse
+    _single_det_spectra = [create_spectra_definition(1, 100.0, 50000.0),   # Chopper pit
+                           create_spectra_definition(2, 100.0, 60000.0),   # Front of Blockhouse
                            create_spectra_definition(3, 3000.0, 70000.0),  # Before Sample
                            create_spectra_definition(4, 3000.0, 70000.0)]  # Point detector
     _multi_det_spectra = [create_spectra_definition(i, 3800.0, 90000.0) for i in range(5, 646)]  # linear detector
 
-    detector = NormalisedIntensityDetector(default_monitor=2, default_detector=3,
+    detector = NormalisedIntensityDetector(default_monitor=2, default_detector=280,
                                            spectra_definitions=_single_det_spectra + _multi_det_spectra)
 
     def __init__(self):
@@ -108,7 +108,7 @@ class PolrefDefaultScan(Defaults):
         from datetime import datetime
         now = datetime.now()
         action_title = info.get("action_title", "unknown")
-        return os.path.join("U:\\", "scripts", "TEST", "{}_{}_{}_{}_{}_{}_{}.dat".format(
+        return os.path.join("U:\\", "TEST", "{}_{}_{}_{}_{}_{}_{}.dat".format(
             action_title, now.year, now.month, now.day, now.hour, now.minute, now.second))
 
 
