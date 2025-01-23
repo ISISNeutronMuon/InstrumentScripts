770cc91b05ce3319f443193d92c1487b0af32d58
spudulike <spudulike@ndxinter.isis.cclrc.ac.uk>
2025-01-23 11:43:33 +0000

    Tidied up __init__.py

--- technique/reflectometry/__init__.py
+++ technique/reflectometry/__init__.py
@@ -1,9 +1,3 @@
-
-# from .base import run_angle, transmission, slit_check, auto_height
-# from .base_New import transmission_new, run_angle_new, run_angle_SM_new, transmission_new_SM
-# from .base_New_v2 import transmission_new_edit, run_angle_new_edit, run_angle_SM_new_edit, transmission_new_SM_edit
-# from .script_actions import *
-#from .contrast_change import contrast_change, inject
 from .sample import SampleGenerator, Sample
 from .script_actions import RunActions, SEActions, DryRun, slit_check, autoheight
 from .NR_motion import _Movement
@@ -19,7 +13,6 @@ go_to_pressure = SEActions.go_to_pressure
 go_to_area = SEActions.go_to_area
 run_angle_store = RunActions.run_angle_store
 auto_height = _Movement.auto_height
-#autoheight = SEActions.autoheight
 
 __all__ = ['SampleGenerator', 'RunActions', 'SEActions', 'DryRun', 'run_angle', 'run_angle_SM', 'slit_check', 'transmission', 'transmission_SM', 'contrast_change', 'inject', 'go_to_pressure','go_to_area', 'run_angle_store', 'auto_height', 'autoheight']
 
